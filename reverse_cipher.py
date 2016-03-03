fd39873b75c918b5bd90b2b5cc180bde7af67cda
Zachary King <kingzach77@gmail.com>
2016-03-02 21:49:27 -0600

    Reverse Cipher

--- reverse_cipher.py
+++ reverse_cipher.py
@@ -2,7 +2,7 @@
 # Reverse Cipher
 # http://inventwithpython.com/hacking (BSD Licensed)
 
-message = 'Three can keep a secret, if two of them are dead.'
+message = input('Enter message: ') 
 translated = ''
 
 i = len(message) - 1
